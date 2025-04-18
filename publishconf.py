8e9a62c0e9c19fb10ff4a0a3d9b6d18de4e23d0b
Alexey Ryabov <alex@b00bl1k.ru>
2025-04-18 23:08:35 +0500

    Allow http

--- publishconf.py
+++ publishconf.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # NOQA
 
-SITEURL = "https://b00bl1k.ru"
+SITEURL = ""
 RELATIVE_URLS = False
 FEED_ALL_ATOM = "feed.xml"
 DELETE_OUTPUT_DIRECTORY = True
